34364f5477410610802e665488266a112ecc1af8
max cheung <maxcheung@optusnet.com.au>
2017-12-15 21:43:57 +1100

    Unit test xml

--- jacksonxml/src/test/java/jacksonxml/SerializeXmlTest.java
+++ jacksonxml/src/test/java/jacksonxml/SerializeXmlTest.java
@@ -1,6 +1,14 @@
 package jacksonxml;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.junit.Test;
 
@@ -14,6 +22,35 @@ public class SerializeXmlTest {
 	    XmlMapper xmlMapper = new XmlMapper();
 	    String xml = xmlMapper.writeValueAsString(new SimpleBean());
 	    assertNotNull(xml);
+	}
+	
+	@Test
+	public void whenJavaGotFromXmlStr_thenCorrect() throws IOException {
+	    XmlMapper xmlMapper = new XmlMapper();
+	    SimpleBean value = 
+	      xmlMapper.readValue("<SimpleBean><x>1</x><y>2</y></SimpleBean>", SimpleBean.class);
+	    assertTrue(value.getX() == 1 && value.getY() == 2);
+	}
+	
+	
+	@Test
+	public void whenJavaGotFromXmlFile_thenCorrect() throws IOException {
+	    File file = new File("src\\test\\simple_bean.xml");
+	    XmlMapper xmlMapper = new XmlMapper();
+	    String xml = inputStreamToString(new FileInputStream(file));
+	    SimpleBean value = xmlMapper.readValue(xml, SimpleBean.class);
+	    assertTrue(value.getX() == 1 && value.getY() == 2);
+	}
+	
+	public static String inputStreamToString(InputStream is) throws IOException {
+	    StringBuilder sb = new StringBuilder();
+	    String line;
+	    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	    while ((line = br.readLine()) != null) {
+	        sb.append(line);
+	    }
+	    br.close();
+	    return sb.toString();
 	}
 
 	
